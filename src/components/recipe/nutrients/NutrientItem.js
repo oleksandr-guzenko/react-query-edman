032ec99029e4e38e44dde9f2b7b99d05b32d1180
dragonwarrior980706 <dragon.warrior980706@gmail.com>
2023-01-12 03:43:43 +0200

    completed

--- src/components/recipe/nutrients/NutrientItem.js
+++ src/components/recipe/nutrients/NutrientItem.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from "react-redux";
-import { 
+import {
     addFilter
- } from "../../../actions/filterActions";
+} from "../../../actions/filterActions";
 
 function NutrientItem({type}) {
-    const [selected, setSelected] = useState(false);
     const [min, setMin] = useState(0);
     const [max, setMax] = useState(100);
+    const filters = useSelector(state => state.filters.filters);
     const dispatch = useDispatch();
+    const selected = filters.findIndex(value => value.type === type) !== -1;
 
     const select = () => {
-        const filter = {
-            type: type,
-            min: min,
-            max: max
+        if(!selected) {
+            const filter = {
+                type: type,
+                min: min,
+                max: max
+            }
+            
+            filters.unshift(filter);
+            dispatch(addFilter(filters));
         }
-
-        dispatch(addFilter(filter));
     }
 
     return (
@@ -28,18 +32,18 @@ function NutrientItem({type}) {
             <span className="small"> {type}</span>
             </button>
             <div className="dropdown-menu p-2">
-            <div className="small text-muted" style={{width: '280px'}}>
-                Choose minimum and maximum values for a desired nutrient per serving. For individual foods, the default serving size is 100 g
-            </div>
-            <div className="fw-bolder text-muted">min</div>
-            <input type="number" className="form-control my-2" value={min} min="0" onChange={e => setMin(e.target.value)} />
-            <div className="fw-bolder text-muted">max</div>
-            <input type="number" className="form-control my-2" value={max} onChange={e => setMax(e.target.value)} />
-            <div className="clearfix">
-                <div className="float-end">
-                <button className="btn btn-outline-secondary rounded-circle btn-sm" onClick={e => select()}><span className="fa fa-plus"></span></button>
+                <div className="small text-muted" style={{width: '280px'}}>
+                    Choose minimum and maximum values for a desired nutrient per serving. For individual foods, the default serving size is 100 g
                 </div>
-            </div>
+                <div className="fw-bolder text-muted">min</div>
+                <input type="number" className="form-control my-2" value={min} min="0" onChange={e => setMin(e.target.value)} />
+                <div className="fw-bolder text-muted">max</div>
+                <input type="number" className="form-control my-2" value={max} onChange={e => setMax(e.target.value)} />
+                <div className="clearfix">
+                    <div className="float-end">
+                    <button className="btn btn-outline-secondary rounded-circle btn-sm" onClick={e => select()}><span className="fa fa-plus"></span></button>
+                    </div>
+                </div>
             </div>
         </div>
     )
